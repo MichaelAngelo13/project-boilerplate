450873a34032b3caa2666e18a7aebc1a637119b6
Michael Garcia <angelo@Michaels-Air.grandenetworks.net>
2023-02-14 11:47:36 -0600

    added express

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+// notice we grab our div with the id root and reassign it to what our app compnent renders
+// we are reading ./index.html
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,4 @@ module.exports = {
       }
     ]
   },
-
-
-
 }
